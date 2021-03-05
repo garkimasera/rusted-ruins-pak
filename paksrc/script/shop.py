fed2dc307f643553ecd9098f7d5328072284b3ee
T. Okubo <t.okubo.rx78+devel@gmail.com>
2021-03-05 20:09:02 +0900

    Fix shop script

--- paksrc/script/shop.py
+++ paksrc/script/shop.py
@@ -1,15 +1,16 @@
 # rusted-ruins-script
 
+import rr
 
 def rr_main():
     while True:
         yield ScriptYield.talk(
             "shop-start", ["shop-ans-buy", "shop-ans-sell", "shop-ans-exit"]
         )
-        c = last_choice()
-        if c == 0:
+        choice = rr.yield_result()
+        if choice == 0:
             yield ScriptYield.shop_buy()
-        elif c == 1:
+        elif choice == 1:
             yield ScriptYield.shop_sell()
         else:
             break
